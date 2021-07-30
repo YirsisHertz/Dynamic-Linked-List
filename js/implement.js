e7962aa9851a16ecf25626ffe86b973edf635073
YirsisHertz <yirsishertz02@gmail.com>
2021-07-30 14:11:50 -0500

    Validacion de algunos errores

--- js/implement.js
+++ js/implement.js
@@ -1,9 +1,14 @@
+const error = document.querySelector("#error");
 const lista = new LinkedList();
 let usuarios;
 
 const createList = () => {
   const firstNodo = document.querySelector("#firstNodo").value;
+  error.style.display = "none";
+
   if (firstNodo.trim() == "") {
+    error.style.display = "block";
+    error.innerHTML = "Debes ingresar un dato";
     return;
   }
 
@@ -13,8 +18,14 @@ const createList = () => {
 };
 
 const addNodo = () => {
+  error.style.display = "none";
   const inputNodo = document.querySelector("#nodo");
-  if (inputNodo.value.trim() == "" || !usuarios) return;
+
+  if (inputNodo.value.trim() == "" || !usuarios) {
+    error.style.display = "block";
+    error.innerHTML = "Debes ingresar un dato valido en una lista valida";
+    return;
+  }
 
   lista.add(new Nodo(inputNodo.value, null));
   lista.print(usuarios);
@@ -25,13 +36,17 @@ const addNodo = () => {
 const insertNodo = () => {
   const positionInsert = document.querySelector("#positionInsert");
   const valueInsert = document.querySelector("#valueInsert");
+  error.style.display = "none";
 
   if (
     positionInsert.value.trim() == "" ||
     valueInsert.value.trim() == "" ||
     !usuarios
-  )
+  ) {
+    error.style.display = "block";
+    error.innerHTML = "Debes agregar un dato valido ";
     return;
+  }
 
   lista.insert(
     parseInt(positionInsert.value),
@@ -45,7 +60,20 @@ const insertNodo = () => {
 
 const deleteNodo = () => {
   const positionDelete = document.querySelector("#positionDelete");
-  if (positionDelete.value.trim() == "" || !usuarios) return;
+  error.style.display = "none";
+
+  if (positionDelete.value.trim() == "" || !usuarios) {
+    error.style.display = "block";
+    error.innerHTML = "Debes borrar un indice valido";
+    return;
+  }
+
+  if (positionDelete.value == "0") {
+    error.style.display = "block";
+    error.innerHTML =
+      "Esto podria eliminar la lista completa y no se recomienda";
+    return;
+  }
 
   lista.delete(parseInt(positionDelete.value));
   lista.print(usuarios);
